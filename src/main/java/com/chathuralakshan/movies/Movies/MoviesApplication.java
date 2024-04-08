b0623356912f2f05025951dbd6f2019b766b4750
chathura226 <chathuralakshan226@gmail.com>
2024-04-08 11:05:55 +0530

    mongo and env

--- src/main/java/com/chathuralakshan/movies/Movies/MoviesApplication.java
+++ src/main/java/com/chathuralakshan/movies/Movies/MoviesApplication.java
@@ -6,15 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@RestController //to tell that this is a rest api
 public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
 
-	@GetMapping("/")
-	public String apiRoot(){
-		return "Hello from Root";
-	}
+
 }
