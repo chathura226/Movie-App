f57f1bc5f050f7ff87e8415a1ccc54aa6095d846
chathura226 <chathuralakshan226@gmail.com>
2024-05-07 15:08:58 +0530

    finalized

--- Frontend/movies/src/api/axiosConfig.js
+++ Frontend/movies/src/api/axiosConfig.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'https://localhost/',
+    baseURL:'http://localhost:8080/',
+    timeout:20000,
+    headers:{
+        "Content-type":"application/json"
+    }
 });
